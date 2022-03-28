c77549af7c92bd139aa6f4077503dacbf190dd4e
99063304 <99063304@mydavinci.nl>
2022-03-28 08:17:01 +0200

    First commit

---
+++ classes/Pokemon.php
@@ -0,0 +1,22 @@
+<?php
+class Pokemon{
+   private  $naam;
+   private  $EnergyType;
+   private  $health;
+   private  $attacks;
+   private  $Weakness;
+   private  $Resistance;
+   private  $hitpoints;
+
+
+  public function __construct(argument){
+
+  }
+  public function Attack(){
+
+  }
+  public function Damage(){
+
+  }
+
+}
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+ require 'classes/Pokemon.php';
